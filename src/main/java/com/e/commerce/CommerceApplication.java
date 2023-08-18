d4a512150753cac3510a77dc02d02c82a0e5bb50
H-JWANNA <hongjjwan@gmail.com>
2023-08-18 16:28:37 +0900

    feat: 설정 파일 수정 및 환경 변수 파일 생성

--- src/main/java/com/e/commerce/CommerceApplication.java
+++ src/main/java/com/e/commerce/CommerceApplication.java
@@ -2,9 +2,11 @@ package com.e.commerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
+@PropertySource("classpath:/env.properties")
 @SpringBootApplication
 public class CommerceApplication {
 
