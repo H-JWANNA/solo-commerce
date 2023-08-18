ee6666411cc22cddef1dee1a3440447b24e0f87d
H-JWANNA <hongjjwan@gmail.com>
2023-08-18 16:31:09 +0900

    refactor: javax -> jakarta 변경

--- src/main/java/com/e/commerce/domain/common/BaseEntity.java
+++ src/main/java/com/e/commerce/domain/common/BaseEntity.java
@@ -2,15 +2,14 @@ package com.e.commerce.domain.common;
 
 import java.time.LocalDateTime;
 
-import javax.persistence.Column;
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
-
 import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 
 @Getter
