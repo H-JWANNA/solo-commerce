bdc6986ad787681cbb19666feefe379a2d89fef6
H-JWANNA <hongjjwan@gmail.com>
2023-08-18 17:41:50 +0900

    fix: Column Default 삭제 (에러 수정)

--- src/main/java/com/e/commerce/domain/common/BaseEntity.java
+++ src/main/java/com/e/commerce/domain/common/BaseEntity.java
@@ -2,7 +2,6 @@ package com.e.commerce.domain.common;
 
 import java.time.LocalDateTime;
 
-import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -18,12 +17,10 @@ import lombok.Getter;
 public class BaseEntity {
 
 	@CreatedDate
-	@ColumnDefault("CURRENT_TIMESTAMP()")
 	@Column(name = "created_at", nullable = false, updatable = false)
 	private LocalDateTime createdAt = LocalDateTime.now();
 
 	@LastModifiedDate
-	@ColumnDefault("CURRENT_TIMESTAMP()")
 	@Column(name = "last_modified_at", nullable = false)
 	private LocalDateTime lastModifiedAt = LocalDateTime.now();
 }
