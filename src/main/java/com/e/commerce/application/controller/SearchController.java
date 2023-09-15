eaee502fe223deac7217173b02d5b4c0813e5d0c
Hong Jeongwan <hongjjwan@gmail.com>
2023-09-15 15:35:58 +0900

    [Product] 상품 검색 구현 (페이지네이션, 템플릿) (#5)
    * feat: entity 생성
    
    * feat: repository 생성
    
    * feat: response body dto 생성
    
    * feat: pagination을 통한 서비스 로직 구현
    
    * feat: controller 구현
    
    * feat: 화면 구성을 위한 템플릿 구현

--- src/main/java/com/e/commerce/application/controller/SearchController.java
+++ src/main/java/com/e/commerce/application/controller/SearchController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.e.commerce.application.dto.MultiResponseDto;
+import com.e.commerce.domain.product.dto.ProductResponseDto;
+import com.e.commerce.domain.product.service.ProductService;
 import com.e.commerce.domain.store.dto.StoreResponseDto;
 import com.e.commerce.domain.store.service.StoreService;
 
@@ -17,6 +19,18 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SearchController {
 	private final StoreService storeService;
+	private final ProductService productService;
+
+	@GetMapping("/products")
+	public String searchProductsForm(
+		@RequestParam(defaultValue = "1") int page,
+		@RequestParam(defaultValue = "10") int size, Model model) {
+		MultiResponseDto<ProductResponseDto> responses = productService.getAllProductsByPaging(page - 1, size);
+		model.addAttribute("productList", responses.data());
+		model.addAttribute("pageInfo", responses.pageInfo());
+
+		return "search-products";
+	}
 
 	@GetMapping("/stores")
 	public String searchStoresForm(
